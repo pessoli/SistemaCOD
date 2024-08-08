d0d8916cd7344786746645bd5ce12d51d9b28e25
lucaspessoli <pessolilucas@gmail.com>
2024-08-08 20:43:50 -0300

    inicializando back e front

---
+++ SistemaCOD/back/src/main/java/com/example/SistemaCOD/SistemaCodApplication.java
@@ -0,0 +1,13 @@
+package com.example.SistemaCOD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCodApplication.class, args);
+	}
+
+}
