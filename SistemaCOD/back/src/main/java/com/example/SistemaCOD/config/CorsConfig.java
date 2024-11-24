6e6435baa2226be735285aabd8ffbc3fa3969ac8
Lucas Pessoli <pessolilucas@gmail.com>
2024-11-24 01:26:24 -0300

    Add files via upload

---
+++ SistemaCOD/back/src/main/java/com/example/SistemaCOD/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.example.SistemaCOD.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")  // Permite CORS para todos os endpoints
+                .allowedOrigins("http://localhost:4200, http://localhost:8081, http://localhost:8081/explore")  // Permite todas as origens, ou especificar como "http://localhost:4200"
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Métodos HTTP permitidos
+                .allowedHeaders("*")  // Permite todos os cabeçalhos
+                .allowCredentials(true);  // Permite credenciais
+    }
+
+}
